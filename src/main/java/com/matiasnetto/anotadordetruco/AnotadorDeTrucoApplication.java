b9b0cd8afd7dacd628ec662add8232fb54b6a89d
Matias Netto <matiasnetto03@gmail.com>
2024-02-25 21:36:10 -0300

    Initial commit

---
+++ src/main/java/com/matiasnetto/anotadordetruco/AnotadorDeTrucoApplication.java
@@ -0,0 +1,13 @@
+package com.matiasnetto.anotadordetruco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnotadorDeTrucoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnotadorDeTrucoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matiasnetto/anotadordetruco/AnotadorDeTrucoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matiasnetto.anotadordetruco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnotadorDeTrucoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
