e38e9d07b2e4876223d21dce2b4afcb9bb4baa0c
Matias Netto <matiasnetto03@gmail.com>
2024-03-12 19:03:08 -0300

    Add user Model, DAO and Service

---
+++ src/main/java/com/matiasnetto/anotadordetruco/Models/User.java
@@ -0,0 +1,16 @@
+package com.matiasnetto.anotadordetruco.Models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class User {
+    private String username;
+    private String fullName;
+    private String passwordHash;
+}
